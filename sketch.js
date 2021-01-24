b4a7c6cc9d8f23592292ebecaec6ac9c04bdcd96
Bouvanni <andris.jfms@gmail.com>
2021-01-24 20:30:20 +0100

    Fix 6 and change link to GitHub

--- sketch.js
+++ sketch.js
@@ -30,16 +30,7 @@ function gotData(data_) {
   q = (windowHeight - 50) / getMaxTotal();
   createBars();
   drawBars();
-  if (isDaily(data[i])) {
-    let div = createDiv(
-      '<svg title="Active every day" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="forestgreen" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>'
-    );
-    div.addClass('badge');
-    div.style('width', width / bars.length + 'px');
-    div.style('left', (i * width) / bars.length + 'px');
-    div.style('top', windowHeight - bars[i].total * q + 'px');
-    div.attribute('title', 'Active everyday');
-  }
+  createCalendars();
 }
 
 function draw() {
@@ -82,7 +73,6 @@ function createBars() {
     bars.push(bar);
   }
 }
-
 function drawBars() {
   rotate(-loaderAngle);
   strokeWeight(config.lineWidth);
@@ -101,6 +91,21 @@ function drawBars() {
   text('500 mi', 5, windowHeight - 500 * q - 20);
   stroke('tomato');
   line(0, windowHeight - 500 * q, width, windowHeight - 500 * q);
+}
+
+function createCalendars() {
+  for (let i = 0; i < data.length; i++) {
+    if (isDaily(data[i])) {
+      let div = createDiv(
+        '<svg title="Active every day" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="forestgreen" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>'
+      );
+      div.addClass('badge');
+      div.style('width', width / bars.length + 'px');
+      div.style('left', (i * width) / bars.length + 'px');
+      div.style('top', windowHeight - bars[i].total * q + 'px');
+      div.attribute('title', 'Active everyday');
+    }
+  }
 }
 
 function getAvgDays(arr) {
